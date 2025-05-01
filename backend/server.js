9d8238fc5c9632b317177d00626a80aafb01c458
Sakshisi3 <sakshisi.1309@gmail.com>
2025-05-01 22:37:19 +0530

    backend changes

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,18 @@
+// const app = require('./app');
+// const mongoose = require('mongoose');
+// require('dotenv').config();
+
+// mongoose.connect(process.env.MONGO_URI)
+//   .then(() => {
+//     app.listen(process.env.PORT, () => {
+//       console.log(`Server running on http://localhost:${process.env.PORT}`);
+//       console.log(`mongodb running`);
+//     });
+//   })
+//   .catch(err => {
+//     console.error('Error connecting to MongoDB:', err.message || err);
+//   });
+  
 const app = require('./app');
 const mongoose = require('mongoose');
 require('dotenv').config();
@@ -6,7 +21,9 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     app.listen(process.env.PORT, () => {
       console.log(`Server running on http://localhost:${process.env.PORT}`);
-      console.log(`mongodb running`);
+      console.log(`MongoDB connected successfully`);
     });
   })
-  .catch(err => console.error(err));
+  .catch(err => {
+    console.error('Error connecting to MongoDB:', err.message || err);
+  });
